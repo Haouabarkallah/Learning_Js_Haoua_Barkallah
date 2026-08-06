4d17fcd84848c87d41fe160950bcc537e06f5441
Haouabarkallah <haouabarkallah@gmail.com>
2026-08-06 16:50:58 +0200

    comment the function

--- day_7/main.js
+++ day_7/main.js
@@ -1,21 +1,23 @@
 // fonction for even numbers
-function evenNumbers(arr) {
-  let evenArr = []; 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 0) {
-            evenArr.push(arr[i]);
-        }
-    }
-    return evenArr;
-}
+// function evenNumbers(arr) {
+//   let evenArr = []; 
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 === 0) {
+//             evenArr.push(arr[i]);
+//         }
+//     }
+//     return evenArr;
+// }
+
+evenNumbers([1, 2, 3, 4, 5, 6]); // [2, 4, 6]
 
 // fonction for odd numbers
-function oddNumbers(arr) {
-  let oddArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 !== 0) {
-            oddArr.push(arr[i]);
-        }
-    }
-    return oddArr;
-}
+// function oddNumbers(arr) {
+//   let oddArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 !== 0) {
+//             oddArr.push(arr[i]);
+//         }
+//     }
+//     return oddArr;
+// }
