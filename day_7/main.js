5ed41399775b80280ff17cdd8cdd8cd37246708f
Haouabarkallah <haouabarkallah@gmail.com>
2026-08-06 16:45:39 +0200

    create a function for odd number

--- day_7/main.js
+++ day_7/main.js
@@ -8,3 +8,14 @@ function evenNumbers(arr) {
     }
     return evenArr;
 }
+
+// fonction for odd numbers
+function oddNumbers(arr) {
+  let oddArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) {
+            oddArr.push(arr[i]);
+        }
+    }
+    return oddArr;
+}
