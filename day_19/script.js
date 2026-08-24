a2a71f65773fe6f1b51191a8f98ad4a9bc04db4c
Haouabarkallah <haouabarkallah@gmail.com>
2026-08-24 11:21:16 +0200

    update the script file

--- day_19/script.js
+++ day_19/script.js
@@ -2,3 +2,8 @@ let enterButton = document.getElementById('enter');
 let againButton = document.getElementById('again');
 let outputText = document.getElementById('output');
 
+let randonNum = Math.ceil(Math.random()* 100);
+
+const result = () => {
+    let inputNum =document.getElementById('inputNumber').value;
+}
