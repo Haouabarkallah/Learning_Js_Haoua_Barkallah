502a47b2f365ebdac0d086fa4fb957a34491525f
Haouabarkallah <haouabarkallah@gmail.com>
2026-08-24 11:37:09 +0200

    adding condition

--- day_19/script.js
+++ day_19/script.js
@@ -6,4 +6,15 @@ let randonNum = Math.ceil(Math.random()* 100);
 
 const result = () => {
     let inputNum =document.getElementById('inputNumber').value;
+   
+    if(inputNum==randonNum){
+        outputText.innerHTML ="It is True" + "The Number is" + randonNum;
+
+    }
+    else if (inputNum > randonNum && inputNum < 100){
+        outputText.innerHTML = "Your Guess is High";
+    }
+    else if (inputNum < randonNum && inputNum > 1){
+        outputText.innerHTML = "Your Guess is Low";
+    }
 }
