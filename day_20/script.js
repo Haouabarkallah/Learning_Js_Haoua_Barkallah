9377eb3b5300be43f67b9050887e2408c3db162a
Haouabarkallah <haouabarkallah@gmail.com>
2026-08-27 17:44:54 +0200

    adding the function updateForm

---
+++ day_20/script.js
@@ -0,0 +1,10 @@
+function updateForm(){
+    //hide all inputs
+    document.getElementById("id_inputs_circle").hidden =true;
+    document.getElementById("id_inputs_triangle").hidden =true;
+    document.getElementById("id_inputs_square").hidden =true;
+    document.getElementById("id_inputs_rectangle").hidden =true;
+    
+    //get the selected shape and show its inputs
+    let shape = ocument
+}
