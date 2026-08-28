bfc03b40784fbb40ec289c455f8adf0167186067
Haouabarkallah <haouabarkallah@gmail.com>
2026-08-28 19:50:06 +0200

    modify the script file

--- day_19/script.js
+++ day_19/script.js
@@ -2,19 +2,20 @@ let enterButton = document.getElementById('enter');
 let againButton = document.getElementById('again');
 let outputText = document.getElementById('output');
 
-let randonNum = Math.ceil(Math.random()* 100);
+let randomNum = Math.ceil(Math.random()* 100);
+console.log(randomNum); // pour vérifier
 
 const result = () => {
     let inputNum = document.getElementById('inputNumber').value;
    
-    if(inputNum==randonNum){
-        outputText.innerHTML ="It is True" + "The Number is " + randonNum;
+    if(inputNum==randomNum){
+        outputText.innerHTML ="It is True" + "The Number is " + randomNum;
 
     }
-    else if (inputNum > randonNum && inputNum < 100){
+    else if (inputNum > randomNum && inputNum < 100){
         outputText.innerHTML = "Your Guess is High";
     }
-    else if (inputNum < randonNum && inputNum > 1){
+    else if (inputNum < randomNum && inputNum > 1){
         outputText.innerHTML = "Your Guess is Low";
     }
     else if (inputNum <1){
