023c51d0d944d8013d4c8a82f3fafe190c33aa42
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-15 17:14:47 +0200

    adding a small function that can convert celcius to fahrenheit

--- day_22/Mini_project_2/script.js
+++ day_22/Mini_project_2/script.js
@@ -1,2 +1,3 @@
 // convertisseur de température
 /* Objectif : pratiquer const/let, template literals (vues plus loin), et une fonction fléchée simple.*/
+const celsiusToFahrenheit = (celsius) => (celsius *9)/5 + 32;
