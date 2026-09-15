1e3aec51ed51835a97d51ef84f3b4a786ae697ee
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-15 14:35:37 +0200

    update the  file

---
+++ day_22/Mini_project_1/script.js
@@ -0,0 +1,6 @@
+//Objectif : vérifier que le lien HTML ↔ JS fonctionne, utiliser console.log, alert, et manipuler un style basique.
+
+console.log("HELLO WORLD FROM JAVASCRIPT!");
+alert("Welcome to my first script js");
+document.body.style.backgroundColor = "#222";
+document.body.style.color ="white";
