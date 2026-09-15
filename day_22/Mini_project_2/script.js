4cdf86814dbb5a1c5528bc50385c4d3a3508552f
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-15 17:29:16 +0200

    update the script

--- day_22/Mini_project_2/script.js
+++ day_22/Mini_project_2/script.js
@@ -1,6 +1,7 @@
 // convertisseur de température
 /* Objectif : pratiquer const/let, template literals (vues plus loin), et une fonction fléchée simple.*/
 const celsiusToFahrenheit = (celsius) => (celsius *9)/5 + 32;
-let temperatureCelsius = 25;
+//let temperatureCelsius = prompt("Entrez la température en degrés Celsius :");// demander à l'utilisateur d'entrer la température en degrés Celsius
+let temperatureCelsius = 27; // valeur par défaut pour les tests
 let temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius);
 console.log(`${temperatureCelsius}°C = ${temperatureFahrenheit}°F`);
