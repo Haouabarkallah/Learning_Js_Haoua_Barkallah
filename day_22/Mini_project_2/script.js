1eea3180e6f7c428383247167d5ce1453428eb9d
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-15 17:20:40 +0200

    set the two variable celsius and fahrenheit

--- day_22/Mini_project_2/script.js
+++ day_22/Mini_project_2/script.js
@@ -1,3 +1,6 @@
 // convertisseur de température
 /* Objectif : pratiquer const/let, template literals (vues plus loin), et une fonction fléchée simple.*/
 const celsiusToFahrenheit = (celsius) => (celsius *9)/5 + 32;
+let temperatureCelsius = 25;
+let temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius);
+console.log(`${temperatureCelsius}°C = ${temperatureFahrenheit}°F`);
