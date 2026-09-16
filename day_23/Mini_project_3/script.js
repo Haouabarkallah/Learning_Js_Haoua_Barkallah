a8cc325f494bab010bffb2ebd4110b192a7ebf69
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-16 11:30:38 +0200

    adding  A small function to calculate the sum of two numbers

---
+++ day_23/Mini_project_3/script.js
@@ -0,0 +1,6 @@
+// A small function to calculate the sum of two numbers using only the variables const and let
+const SumAandB =(sum) => (a + b);
+let a = 5; // first number
+let b = 10; // second number
+let sum = SumAandB(); // calculate the sum
+console.log(`The sum of ${a} and ${b} is ${sum}`); // output the result
