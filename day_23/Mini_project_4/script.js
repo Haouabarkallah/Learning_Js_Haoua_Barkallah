a4d36879a8e743ec12536994007f081a17667c22
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-16 18:39:11 +0200

    modify the script

--- day_23/Mini_project_4/script.js
+++ day_23/Mini_project_4/script.js
@@ -1,4 +1,4 @@
-// types of data in js
+//  data types in js
 let n1= 1  // int/number
 let n2= 1.3 // number because js does not have float or double
 let n3= 10000000  // bigint
@@ -9,3 +9,5 @@ let c // undefined
 let isNull = null // null
 let notes =[1, 13,0, "pas de notes", [3,8,"les bonus"]] // array
 let student = {firstname:"Ali", age: 20, isStudent:true, maths:13} // object
+let sym = Symbol("id") // symbol
+console.log(`Data types in js are:`)
