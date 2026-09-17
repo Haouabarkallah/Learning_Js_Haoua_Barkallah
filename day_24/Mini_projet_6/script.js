63ede1808efb160a0a34cc801dd8e58189b2aa3b
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-17 15:31:41 +0200

    update the function

--- day_24/Mini_projet_6/script.js
+++ day_24/Mini_projet_6/script.js
@@ -3,11 +3,11 @@ function calculatrice(a, operateur, b) {
     switch (operateur) {
         case "+":
             return a + b;
-        case "-": 
-            return a - b;    
-        case "*": 
+        case "-":
+            return a - b;
+        case "*":
             return a * b;
-        case "/": 
+        case "/":
             return  b !== 0 ? a / b : "Erreur : Division par zéro";
         default:
             return " Opérateur inconnu. Veuillez utiliser +, -, * ou /.";
@@ -15,6 +15,6 @@ function calculatrice(a, operateur, b) {
     }
 }
 
-// console.log(calculatrice(10, "+", 5)); // Affiche 15
-// console.log(calculatrice(10, "/", 0)); // Affiche "Erreur : Division par zéro"
-console.log("veuillez entre les valeurs a, operateur et b pour effectuer le calcul");
+console.log(calculatrice(10, "+", 5)); // Affiche 15
+console.log(calculatrice(10, "/", 0)); // Affiche "Erreur : Division par zéro"
+// console.log("veuillez entre les valeurs a, operateur et b pour effectuer le calcul");
