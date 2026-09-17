6356a9c959ca31955f8bc29245d9ad5c4244c705
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-17 16:51:32 +0200

    modify the function

--- day_24/Mini_project_7/script.js
+++ day_24/Mini_project_7/script.js
@@ -1,6 +1,7 @@
 //Mini-projet 7 : « Générateur de table de multiplication »
-function tableDeMultiplication(nombre, max =10){
+function tableDeMultiplication(nombre, max =12){
     for (let i = 1; i <= max; i++) {
         console.log(`${nombre} x ${i} = ${nombre * i}`);
     }
 } 
+tableDeMultiplication(5); //appel de la fonction avec le nombre 5 et la valeur par défaut de max (12)
