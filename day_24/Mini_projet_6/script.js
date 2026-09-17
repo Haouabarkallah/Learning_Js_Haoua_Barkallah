bd7441821a963d954d2a293f5604a9237fa3cf47
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-17 15:19:14 +0200

    update the function

--- day_24/Mini_projet_6/script.js
+++ day_24/Mini_projet_6/script.js
@@ -1,6 +1,14 @@
 //Mini-projet 4 : « Calculatrice en console »
 function calculatrice(a,b, operateur) {
     switch (operateur) {
-        
+        case "+":
+            return a + b;
+        case "-": 
+            return a - b;    
+        case "*": 
+            return a * b;
+        case "/": 
+            return  b !== 0 ? a / b : "Erreur : Division par zéro";
+
     }
 }
