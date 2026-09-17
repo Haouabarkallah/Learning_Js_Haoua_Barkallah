f7fb50f1da0a66ff70e7269214a52c27f34c2e7a
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-17 13:54:42 +0200

    update the function

--- day_24/Mini_project_5/script.js
+++ day_24/Mini_project_5/script.js
@@ -1,7 +1,7 @@
 // Mini project 5 : testeur de types
 function analyser(valeur){
-    console.log(`valeur :${JSON.stringify(valeur)}`);
-    console.log(`type : ${typeof valeur}`);
+    console.log(`Taleur :${JSON.stringify(valeur)}`);
+    console.log(`Type : ${typeof valeur}`);
     console.log(`Truthy ? ${Boolean(valeur)}`);
 
 }
