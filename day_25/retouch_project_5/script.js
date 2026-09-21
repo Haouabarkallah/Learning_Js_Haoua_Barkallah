bb80224856c2a36a9841eb7a609efdcefd8ad49b
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-21 11:06:59 +0200

    modify the function

--- day_25/retouch_project_5/script.js
+++ day_25/retouch_project_5/script.js
@@ -1,4 +1,4 @@
-// Mini project 5 : testeur de types
+// retouching Mini project 5 : testeur de types
 function analyser(valeur){
     console.log(`Valeur :${JSON.stringify(valeur)}`);
     console.log(`Type : ${typeof valeur}`);
