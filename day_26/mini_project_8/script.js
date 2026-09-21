5579afc4b1dd5362a0d83fa426099755989a0bda
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-21 16:31:30 +0200

    adding the vars

--- day_26/mini_project_8/script.js
+++ day_26/mini_project_8/script.js
@@ -1,1 +1,4 @@
 // Mini-projet 8 : « Devine le nombre »
+
+const nombreSecret = Math.floor(Math.random() * 100) + 1; // Génère un nombre aléatoire entre 1 et 100
+const tentatives =[45,70,60,65,63]; // Exemple de tentatives pour tester le code
