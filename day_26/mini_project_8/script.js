75af3f93fc5be8e119a4b447ef02452de55c0341
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-22 17:30:38 +0200

    modify the var

--- day_26/mini_project_8/script.js
+++ day_26/mini_project_8/script.js
@@ -1,6 +1,6 @@
 // Mini-projet 8 : « Devine la lettre »
-
-const lettre= 'H'; // La lettre secrète à deviner
+const lettre = 5; // La lettre secrète à deviner
+// // const lettre= 'H'; // La lettre secrète à deviner
 let lettreDeviner; // La lettre que l'utilisateur va deviner
 for (let i=lettreDeviner; i !== lettre; i--) { // Boucle jusqu'à ce que l'utilisateur devine la lettre) 
    let lettreDeviner = prompt("Devinez la lettre secrète (entre A et Z) :") * 1; // Demande à l'utilisateur de deviner la lettre
@@ -16,4 +16,3 @@ for (let i=lettreDeviner; i !== lettre; i--) { // Boucle jusqu'à ce que l'utili
 
     
 }
-// console.log(`Le nombre secret était ${nombreSecret}.`); // Affiche le nombre secret à la fin du jeu
