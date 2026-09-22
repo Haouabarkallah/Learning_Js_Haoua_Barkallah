f2f260a76fb477361d53117cfd35c84024969b1f
Haouabarkallah <haouabarkallah@gmail.com>
2026-09-22 16:44:18 +0200

    initialize the loop

--- day_26/mini_project_8/script.js
+++ day_26/mini_project_8/script.js
@@ -1,16 +1,8 @@
 // Mini-projet 8 : « Devine la lettre »
 
-const lettre= 'a'; // La lettre secrète à deviner
+const lettre= 'H'; // La lettre secrète à deviner
 let lettreDeviner; // La lettre que l'utilisateur va deviner
-for (const tentative of tentatives) {
-    if (tentative === nombreSecret) {
-        console.log(`Trouvé ! Le nombre secret était ${nombreSecret}.`); // L'utilisateur a trouvé le nombre secret
-        break; // Sort de la boucle si le nombre est trouvé 
-    }else if (tentative < nombreSecret) {
-        console.log(`Le nombre secret est plus grand que ${tentative}.`); // Le nombre secret est plus grand que la tentative
-    }else {
-        console.log(`Le nombre secret est plus petit que ${tentative}.`); // Le nombre secret est plus petit que la tentative
-    }
-
+for (let i=lettreDeviner; i !== lettre; i--) { // Boucle jusqu'à ce que l'utilisateur devine la lettre) {
+   let lettreDeviner = prompt("Devinez la lettre secrète (entre A et Z) :"); // Demande à l'utilisateur de deviner la lettre
 }
 // console.log(`Le nombre secret était ${nombreSecret}.`); // Affiche le nombre secret à la fin du jeu
